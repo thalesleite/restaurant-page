0cb19d77efe6e6a19461c646460315ea001c0d37
thalesleite <thalesaleite@gmail.com>
2024-05-21 16:12:56 +0100

    add menu and about content

--- src/about.js
+++ src/about.js
@@ -8,5 +8,25 @@ export default function about() {
   headline.textContent = "About"
   divHeadline.appendChild(headline)
 
+  const divAbout = document.createElement("div")
+  divHeadline.classList.add("about")
+  const paragraphOne = document.createElement("p")
+  paragraphOne.textContent =
+    "Welcome to Le Gourmet Haven, where culinary excellence meets unparalleled hospitality. Our restaurant is a sanctuary for food lovers, offering an exquisite dining experience that marries the art of fine dining with the comfort of a welcoming atmosphere."
+  const paragraphTwo = document.createElement("p")
+  paragraphTwo.textContent =
+    "At Le Gourmet Haven, we believe that every meal is a celebration of life, and we strive to create moments that linger in your memory long after the last bite. Our talented chefs are dedicated to sourcing the freshest, highest-quality ingredients to craft dishes that are as visually stunning as they are delicious. Each plate is a testament to our commitment to innovation, tradition, and, above all, passion for the culinary arts."
+  const paragraphThree = document.createElement("p")
+  paragraphThree.textContent =
+    "Our elegantly designed dining space provides the perfect backdrop for any occasion, from intimate dinners to grand celebrations. With a focus on exceptional service, our attentive staff ensures that every detail is attended to, allowing you to relax and savor your time with us."
+  const paragraphFour = document.createElement("p")
+  paragraphThree.textContent =
+    "We invite you to join us at Le Gourmet Haven, where every meal is more than just dining—it’s an experience. Discover the flavors, ambiance, and impeccable service that have made us a beloved destination for discerning diners. Thank you for choosing Le Gourmet Haven, where your culinary dreams come true."
+  divAbout.appendChild(paragraphOne)
+  divAbout.appendChild(paragraphTwo)
+  divAbout.appendChild(paragraphThree)
+  divAbout.appendChild(paragraphFour)
+
   content.appendChild(divHeadline)
+  content.appendChild(divAbout)
 }
--- src/menu.js
+++ src/menu.js
@@ -8,5 +8,66 @@ export default function menu() {
   headline.textContent = "Menu"
   divHeadline.appendChild(headline)
 
+  const appetizers = document.createElement("h2")
+  appetizers.textContent = "APPETIZERS"
+  const lobster = createRecipe(
+    "Lobster Bisque",
+    "Rich and creamy, garnished with a touch of brandy and fresh herbs."
+  )
+  const truffle = createRecipe(
+    "Truffle Arancini",
+    "Crispy risotto balls infused with truffle oil, served with a garlic aioli dip."
+  )
+  const foie = createRecipe(
+    "Foie Gras Crostini",
+    "Seared foie gras on toasted baguette slices, topped with fig compote."
+  )
+
+  const entrees = document.createElement("h2")
+  entrees.textContent = "ENTREES"
+  const filet = createRecipe(
+    "Filet Mignon",
+    "Pan-seared scallops with a saffron risotto and a beurre blanc sauce."
+  )
+  const seared = createRecipe(
+    "Seared Scallops",
+    "Crispy risotto balls infused with truffle oil, served with a garlic aioli dip."
+  )
+  const mushroom = createRecipe(
+    "Wild Mushroom Risotto",
+    "Creamy Arborio rice with a medley of wild mushrooms, parmesan cheese, and a hint of white truffle oil."
+  )
+  const roast = createRecipe(
+    "Roast Duck Breast",
+    "Succulent duck breast with a cherry gastrique, served with roasted fingerling potatoes and braised red cabbage."
+  )
+
   content.appendChild(divHeadline)
+
+  content.appendChild(appetizers)
+  content.appendChild(truffle)
+  content.appendChild(foie)
+  content.appendChild(lobster)
+
+  content.appendChild(entrees)
+  content.appendChild(filet)
+  content.appendChild(seared)
+  content.appendChild(mushroom)
+  content.appendChild(roast)
+}
+
+function createRecipe(textHeader, textDescription) {
+  const divRecipe = document.createElement("div")
+  divRecipe.classList.add("recipe")
+
+  const header = document.createElement("h3")
+  header.textContent = textHeader
+
+  const description = document.createElement("p")
+  description.textContent = textDescription
+
+  divRecipe.appendChild(header)
+  divRecipe.appendChild(description)
+
+  return divRecipe
 }
